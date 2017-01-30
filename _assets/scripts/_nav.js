9949111b19d4489fce8ca21e8d3e3934bfe65835
Rene Dellefont <renedellefont@gmail.com>
2017-01-30 18:29:15 -0500

    Add Smooth Scroll functionality

---
+++ _assets/scripts/_nav.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var $ = require('jquery');
+var ss = require('smooth-scroll');
+var gum = require('gumshoe');
+
+console.log('nav js loaded');
+ss.init();
--- _assets/scripts/main.js
+++ _assets/scripts/main.js
@@ -4,3 +4,5 @@
  *
  * require('./FILENAME.js');
  */
+
+require('./_nav.js');
