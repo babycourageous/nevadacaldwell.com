2ff36f126a636b0cad78d350ece20d7f596a72d6
Rene Dellefont <renedellefont@gmail.com>
2019-02-25 16:33:36 -0500

    Delete all gulp

--- gulp/tasks/clean:scripts.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(gulp, plugins, config) {
-	return function() {
-		console.log('---> clean:scripts');
-		return plugins.del(config.scripts.jekylldest + 'main.min.js');
-  };
-};
