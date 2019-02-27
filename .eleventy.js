8f293c72335aae6852d2aeb31bff1cdb36404f0c
Rene Dellefont <renedellefont@gmail.com>
2019-02-27 14:01:32 -0500

    Initial Netlify CMS admin creation.

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,12 @@ module.exports = function(eleventyConfig) {
   // Aliases are in relation to the _includes folder
   eleventyConfig.addLayoutAlias('default', 'layouts/default.njk')
 
+  eleventyConfig.addPassthroughCopy('admin')
+
   let markdownIt = require('markdown-it')
   let options = {
     html: true,
   }
-
   eleventyConfig.setLibrary('md', markdownIt(options))
 
   return {
