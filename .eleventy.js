2af040dc109b713b929166b4b73816cc2f62fcb8
Rene Dellefont <renedellefont@gmail.com>
2019-03-01 15:58:20 -0500

    Add eleventy filter and shortcode for converting markdown in data file.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,24 @@
+const marked = require('marked')
+
 module.exports = function(eleventyConfig) {
   // Aliases are in relation to the _includes folder
   eleventyConfig.addLayoutAlias('default', 'layouts/default.njk')
+
+  eleventyConfig.addFilter('md', function(value) {
+    let result
+    try {
+      result = marked(value).trim()
+      result = result.replace(/^<p>|<\/p>$/g, '')
+      return result
+    } catch (e) {
+      console.error('Error processing markdown:', e)
+      return value
+    }
+  })
+
+  eleventyConfig.addShortcode('texttohtml', function(text) {
+    return `${text}`
+  })
 
   eleventyConfig.addPassthroughCopy('admin')
 
